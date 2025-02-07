c705292b571d38269a99c62ea1609bfa82d57d00
Slimanyy <aabdulqudduss@gmail.com>
2025-02-07 12:46:05 +0100

    added ignit module for erc 20

---
+++ ignition/modules/Token.ts
@@ -0,0 +1,18 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+const TokenkModule = buildModule("token", (m) => {
+     const name = m.getParameter("name", "Slimany");
+     const symbol = m.getParameter("symbol", "SLM");
+     // const decimals = m.getParameter("decimals", 18);
+     const tokenSupply = m.getParameter("tokenAmount", 6420);
+
+  const Token = m.contract("ERC20", [name, symbol, tokenSupply]);
+
+  return { Token };
+});
+
+export default TokenkModule;
